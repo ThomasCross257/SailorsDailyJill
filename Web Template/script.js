f69b101d7fa44e103ff17c927dcbca7a98fc2357
Ph00sant <thomascross257@gmail.com>
2023-02-20 13:35:14 -0800

    Updated Database CSS and JS

--- Web Template/script.js
+++ Web Template/script.js
@@ -10,3 +10,16 @@ function loginSwap(){
         signUp.style.display = "none";
     }
 }
+function DB_EntrySwap(ID){
+    var entry_id = ID;
+    console.log(entry_id);
+    if (entry_id == "Submit" || entry_id == "Return"){
+        document.getElementById("new_Entry").style.display = "none"; 
+        document.getElementById("db_Broad").style.display = "block";
+        
+    }
+    else if (entry_id = "Enter"){
+        document.getElementById("db_Broad").style.display = "none";
+        document.getElementById("new_Entry").style.display = "block"; 
+    }
+}
