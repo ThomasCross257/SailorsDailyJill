6e127dbc6ee0d24eac797e74b752f61a1d729f5f
Ph00sant <thomascross257@gmail.com>
2023-02-21 11:12:04 -0800

    Further restructuring, Addition of new pages, Expansion of Flask App

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -6,14 +6,36 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        user = request.form["signin_user"]
+        password = request.form["signin_password"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    if request.method == "POST":
+        user = request.form["signup_user"]
+        password = request.form["signup_password"]
+        email = request.form["signup_email"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("signup.html")
 
 @app.route("/database")
 def database():
     return render_template("database.html")
 
+@app.route("/redirect")
+def redirect():
+    return render_template("redirect.html")
+
+@app.route("/<usr>Home")
+def user(usr):
+    return render_template("userpage.html")
 
 if __name__ == "__main__":
     app.run()
--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,3 @@
-function loginSwap(){
-    var signIn = document.getElementById("signin");
-    var signUp = document.getElementById("signup");
-    if (signIn.style.display == "block"){
-        signIn.style.display = "none";
-        signUp.style.display = "block";
-    }
-    else{
-        signIn.style.display = "block";
-        signUp.style.display = "none";
-    }
-}
 function DB_EntrySwap(ID){
     var entry_id = ID;
     console.log(entry_id);
